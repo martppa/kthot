9c3b239042e3536ef5ac96f649a991fbe82673ab
dieppa <hm.dieppa@gmail.com>
2025-07-21 08:55:52 +0200

    NegatedOperation no longer accepts ReflexiveOperation but Operation

--- kotlin-js-dsl/src/main/kotlin/net/asere/kotlin/js/dsl/syntax/operation/NegatedOperation.kt
+++ kotlin-js-dsl/src/main/kotlin/net/asere/kotlin/js/dsl/syntax/operation/NegatedOperation.kt
@@ -3,10 +3,9 @@ package net.asere.kotlin.js.dsl.syntax.operation
 import net.asere.kotlin.js.dsl.syntax.operation.operator.Negate
 import net.asere.kotlin.js.dsl.syntax.operation.operator.Operator
 
-class NegatedOperation<T : ReflexiveOperation>(
+class NegatedOperation<T : Operation>(
     internal val comparison: T,
 ) : ReflexiveOperation() {
     override val leftSideElement: Operator = Negate
     override val rightSideElement: Operation = comparison.group()
-
 }
