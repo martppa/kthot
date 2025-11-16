2f389493d27672abde3a324e36721f844d45c204
dieppa <hm.dieppa@gmail.com>
2025-11-16 10:17:38 +0100

    Added generation files for testing

---
+++ src/main/kotlin/net/asere/kthot/js/dsl/sample/test/ApiTestFunctionModuleJsDefinition.kt
@@ -0,0 +1,47 @@
+package net.asere.kthot.js.dsl.sample.test
+
+import net.asere.kthot.js.dsl.JsNothing
+import net.asere.kthot.js.dsl.ksp.annotation.JsApiFunctionModule
+import net.asere.kthot.js.dsl.ksp.annotation.JsFunction
+import net.asere.kthot.js.dsl.type.array.JsArray
+import net.asere.kthot.js.dsl.type.number.JsNumber
+import net.asere.kthot.js.dsl.type.promise.JsPromise
+import net.asere.kthot.js.dsl.type.string.JsString
+import net.asere.kthot.js.dsl.type.value.JsValue
+
+@JsApiFunctionModule(name = "JsApiTestFunctionModule")
+interface ApiTestFunctionModuleJsDefinition {
+
+    @JsFunction
+    fun returningFunction(): JsNothing
+
+    @JsFunction
+    fun functionWithParameter(number: JsNumber): JsNothing
+
+    @JsFunction
+    fun functionWith2Parameter(number: JsNumber, string: JsString): JsNothing
+
+    @JsFunction
+    fun <T : JsValue> functionWithGenericParameter(parameter: T): JsNothing
+
+    @JsFunction
+    fun <T : JsValue> basicFunctionReturnGeneric(): T
+
+    @JsFunction
+    fun functionReturningGenericTypeParameter(): JsArray<JsString>
+
+    @JsFunction
+    fun functionWithGenericTypeParameterStringAndReturn(genericTyped: JsPromise<JsString>): JsArray<JsString>
+
+    @JsFunction
+    fun functionWithGenericTypeParameterString(genericTyped: JsPromise<JsString>): JsNothing
+
+    @JsFunction
+    fun <T : JsValue> functionWithGenericTypeParameter(genericTyped: JsPromise<T>): JsNothing
+
+    @JsFunction
+    fun functionWithComplexGenericParameterString(genericTyped: JsPromise<JsArray<JsString>>): JsNothing
+
+    @JsFunction
+    fun <T : JsValue> functionWithComplexGenericTypeParameter(genericTyped: JsPromise<JsArray<T>>): JsNothing
+}
